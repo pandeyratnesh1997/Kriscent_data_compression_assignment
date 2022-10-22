6762ec3466d9e5d31b5e76c4440ab4b39aa8c4c6
Ratnesh <pandeyratnesh1997@gmail.com>
2022-10-22 13:54:12 +0530

    frontend and compressed data storage folder added

---
+++ Frontend/profile_app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Register from './Components/Register';
+import Login from './Components/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Register/>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/profile_app/src/Components/Register.jsx
@@ -0,0 +1,146 @@
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import FormData from 'form-data'
+import { Button, Heading, useToast } from "@chakra-ui/react";
+import { Link, useNavigate } from "react-router-dom";
+
+import styles from "../Styled/form.module.css";
+
+
+const formData = new FormData();
+const Register = () => {
+  const [userDetails, setUserDetails] = useState({
+    name: "",
+    mobile: "",
+    password: "",
+    image: "",
+  });
+
+ 
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+     
+    setUserDetails({ ...userDetails, [name]: value });
+  };
+
+  const handleFile = (e) =>{
+
+    setUserDetails({...userDetails, image : e.target.files[0]})
+  }
+
+  const handleRegister =  async(e) => {
+
+    e.preventDefault();
+   
+     console.log(userDetails,formData);
+    formData.set('name' , userDetails.name);
+    formData.set('password' , userDetails.password);
+    formData.set('mobile' , userDetails.mobile);            
+    formData.set('image' , userDetails.image);            
+    console.log(formData);
+    // try {
+    //   let response = await axios.post(
+    //     "http://localhost:5000/user/signup",
+    //     userDetails
+      
+    //   );
+    //   console.log(response);
+    //   //   navigate("/login");
+    // } catch (error) {
+    //   console.log(error);
+    //   alert("Error in Register");
+    // }
+  };
+
+  return (
+    <div className={styles.container}>
+      <Heading size={"md"}>Register</Heading>
+      <form
+        onSubmit={handleRegister}
+       
+        encType="multipart/form-data"
+      >
+        <div className={styles.row}>
+          <div className={styles.label_column}>
+            <label className={styles.label}>Name</label>
+          </div>
+          <div className={styles.input_column}>
+            <input
+              className={styles.input}
+              type="text"
+              placeholder="Enter your name.."
+              name="name"
+              value={userDetails.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div className={styles.label_column}>
+            <label className={styles.label}>Mobile</label>
+          </div>
+          <div className={styles.input_column}>
+            <input
+              className={styles.input}
+              type="number"
+              placeholder="Enter your email id.."
+              name="mobile"
+              value={userDetails.mobile}
+              onChange={handleChange}
+              required
+            />
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div className={styles.label_column}>
+            <label className={styles.label}>Password</label>
+          </div>
+          <div className={styles.input_column}>
+            <input
+              className={styles.input}
+              type="password"
+              placeholder="Enter your password.."
+              name="password"
+              value={userDetails.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div className={styles.label_column}>
+            <label className={styles.label}>Profile Picture</label>
+          </div>
+          <div className={styles.input_column}>
+            <input
+              className={styles.input}
+              type="file"
+              name="image"
+              onChange={handleFile}
+             
+            />
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <input className={styles.submit} type="submit" value="Register" />
+        </div>
+        <div className={styles.row}>
+          <p style={{ margin: "10px" }}>Already Registered</p>
+          <br />
+          <Button style={{ margin: "10px" }}>
+            <Link to="/login">Go to Login</Link>
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
